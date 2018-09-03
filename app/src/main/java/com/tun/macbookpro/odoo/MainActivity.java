e62c86452e627aa27c6e23af17cd22d1e17cfd5a
macbookpro <minhthuvlt@gmail.com>
2018-09-03 21:11:29 +0700

    Initial commit

---
+++ app/src/main/java/com/tun/macbookpro/odoo/MainActivity.java
@@ -0,0 +1,102 @@
+package com.tun.macbookpro.odoo;
+
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private String myUrl = "http://14.243.50.8:8069/web/login";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView)findViewById(R.id.mWebView);
+        if(ConnectionUtil.isConnected(this)){
+            setUpWebView();
+        }else{
+            Toast.makeText(this,"Vui lòng kiểm tra kết nối internet", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void setUpWebView() {
+            mWebView.setWebViewClient(new MyBrWebView());
+            mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+            mWebView.getSettings().setLoadsImagesAutomatically(true);
+
+            mWebView.getSettings().setJavaScriptEnabled(true);
+            mWebView.getSettings().setBuiltInZoomControls(true);
+            mWebView.getSettings().setSupportZoom(true);
+            mWebView.loadUrl(myUrl);
+
+
+    }
+
+    private class MyBrWebView extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            view.loadUrl(url);
+            return true;
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (mWebView.canGoBack()) {
+                        mWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        hideSystemUI(hasFocus);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void hideSystemUI(boolean hasFocus) {
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Window window = getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(getColor(R.color.colorPrimary));
+            }
+        }
+    }
+}
